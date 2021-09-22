f432541e4b4f008f2e495437eb9c5d611ad0b26c
ROUIJEL <nabil.rouijel@capgemini.com>
2021-09-22 02:52:56 +0200

    Full pipeline test 3

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('salamo alikom'));
+app.get('/', (req, res) => res.send('ROUIJEL NABIL'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
